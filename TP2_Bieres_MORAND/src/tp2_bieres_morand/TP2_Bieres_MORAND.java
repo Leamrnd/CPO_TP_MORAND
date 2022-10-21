62cde59719ef7ddfb8db86aa302796aa74099d01
Lea Morand <lea.morand@epfedu.fr>
2022-10-21 16:51:48 +0200

    Création d'un nouveau projet versionné jusqu'à la question 14 (fin partie 1)

--- TP2_Bieres_MORAND/src/tp2_bieres_morand/TP2_Bieres_MORAND.java
+++ TP2_Bieres_MORAND/src/tp2_bieres_morand/TP2_Bieres_MORAND.java
@@ -17,9 +17,24 @@ public class TP2_Bieres_MORAND {
         // TODO code application logic here
         BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des Trolls", 7.0, "Dubuisson");
         uneBiere.lireEtiquette();
+        System.out.println(uneBiere);
         
         BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
         autreBiere.lireEtiquette();
+        autreBiere.Décapsuler();
+        
+        BouteilleBiere Biere3 = new BouteilleBiere ("Desperados", 9.0, "Sous les Tropiques");
+        Biere3.lireEtiquette();
+        
+        BouteilleBiere Biere4 = new BouteilleBiere ("Unicorn", 5.5,"Foret Enchantée");
+        Biere4.lireEtiquette();
+        Biere4.Décapsuler();
+        System.out.println(Biere4);
+        
+        BouteilleBiere Biere5 = new BouteilleBiere ("Guiness", 4.2, "Abbaye Guiness");
+        Biere5.lireEtiquette();
+        
+        
     }
 
 }
