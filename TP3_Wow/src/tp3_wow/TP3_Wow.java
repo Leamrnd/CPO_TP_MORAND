c62a2f42822b1bf705f5eba2d5b49c9392f14497
Lea Morand <lea.morand@epfedu.fr>
2022-11-08 16:14:05 +0100

    tp3 jusqua question 10

--- TP3_Wow/src/tp3_wow/TP3_Wow.java
+++ TP3_Wow/src/tp3_wow/TP3_Wow.java
@@ -15,6 +15,16 @@ public class TP3_Wow {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Epée Excalibur = new Epée("Excalibur", 7,5);
+        System.out.println(Excalibur);
+        
+        Epée Durandal = new Epée("Durandal", 4,7);
+        System.out.println(Durandal);
+        
+        
+        Baton Baton1 = new Baton("Chêne", 4,5);
+        Baton Baton2 = new Baton("Charme", 5,6);
+        System.out.println(Baton1);
     }
     
 }
