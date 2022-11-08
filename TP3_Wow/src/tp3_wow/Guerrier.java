a540519e0534f78757b826160dd1e9ea59bafc8d
Lea Morand <lea.morand@epfedu.fr>
2022-11-08 17:15:55 +0100

    tp3, creation class personnage, magicien et guerrier

---
+++ TP3_Wow/src/tp3_wow/Guerrier.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_wow;
+
+/**
+ *
+ * @author leamo
+ */
+public class Guerrier extends Personnage{
+    boolean cheval;
+    
+    public Guerrier(String n , int i, boolean c) {
+        super(n,i);
+        cheval = c;
+    }
+    
+}
